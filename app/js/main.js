7fe6744a6ec8bf4be530b1e48bca1705b6038821
LehaKos <lehakos@gmail.com>
2016-02-06 01:49:44 +0300

    add tooltips for form fields

--- app/js/main.js
+++ app/js/main.js
@@ -1,60 +1,18 @@
+//=============================================
+// APP MODULE
+//=============================================
+
 var app = (function() {
 
     // Функция инициализации
     var init = function() {
         _setupListeners();
-        _addPlaceholdersToOldBrowsers();
-        _styleInputFile();
     };
 
     // Прослушивает события
     var _setupListeners = function() {
         _setupPopups();
     };
-
-    // Добавляем placeholder в те браузеры, где он не поддерживается
-    var _addPlaceholdersToOldBrowsers = function() {
-
-        if ($('input, textarea').length) {
-            $('input, textarea').placeholder();
-        }
-
-    };
-
-    // Стилизация input[type="file"]
-    var _styleInputFile = function() {
-
-        var inputFile = $('.input-file');
-
-        if (inputFile.length) {
-
-            inputFile.each( function() {
-
-                var $this = $(this),
-                    $defaultVal = $this.text(),
-                    $textContainer = $this.find('.input-file__text'),
-                    $hiddenInput = $this.find('.input-file__hidden');
-
-                // Добавляем название выбранного файла в муляж инпута
-                $hiddenInput.on('change', function(e) {
-
-                    var fileName = e.target.value.split('\\').pop();
-
-                    if(fileName) {
-                        $textContainer.text(fileName);
-
-                        if (!$this.hasClass('input-file_non-empty')) {
-                            $this.addClass('input-file_non-empty');
-                        }
-                    }
-
-                });
-
-            });
-
-        }
-
-    }
 
     // Работа с попапами
     var _setupPopups = function() {
@@ -150,15 +108,15 @@ var app = (function() {
             $elem = $(selector);
 
         if ($elem.hasClass('no-scroll')) {
-          $elem.css({
-            'padding-right': scrollBarWidth + 'px'
-          });
+            $elem.css({
+                'padding-right': scrollBarWidth + 'px'
+            });
 
         } else {
 
-          $elem.css({
-            'padding-right': ''
-          });
+            $elem.css({
+                'padding-right': ''
+            });
         }
 
     }
@@ -172,8 +130,227 @@ var app = (function() {
 
 })();
 
+//=============================================
+// FORMS MODULE
+//=============================================
+
+var formsModule = (function() {
+
+    // Функция инициализации
+    var init = function() {
+        _setupListeners();
+        _styleInputFile();
+        _addPlaceholdersToOldBrowsers();
+        _setupTooltip();
+    }
+
+    // В этой функции запускаются все обработчики событий
+    var _setupListeners = function() {
+        _setupForms();
+    };
+
+    // Функция для валидации полей форм
+    var validateFields = function($validationFields) {
+
+        var valid = true;
+
+        $validationFields.each(function() {
+
+            var $this = $(this);
+
+            var validateThis = function() {
+                validateFields($this);
+            }
+
+            if (!$this.val()) {
+                if ($this.hasClass('valid')) {
+                    $this.removeClass('valid');
+                }
+
+                $this
+                    .addClass('invalid')
+                    .trigger('validationFail')
+
+                // Если поле не прошло валидацию, вешаем на него обработчик
+                // который будет валидировать поле при введении в нем символов
+                // или при изменении его значения
+                $this.on('change keyup', validateThis);
+
+                valid = false;
+
+            } else {
+
+                if ($this.hasClass('invalid')) {
+                    $this
+                        .toggleClass('invalid valid')
+                        .trigger('validation');
+                }
+
+                // если поле прошло валидацию, то убираем с него персональную валидацию
+                $this.off('change keypress', validateThis);
+            }
+
+        });
+
+        return valid;
+    };
+
+    // настраиваем тултипы для полей формы
+    var _setupTooltip = function() {
+
+        // ищем все поля с атрибутом required
+        var $form = $('form'),
+            $requiredFields = $form.find('[required]');
+
+        if ($requiredFields.length) {
+
+            $requiredFields.each(function() {
+
+                var $this = $(this),
+                    $text = $this.data('tooltip-text') || 'Пожалуйста, заполните это поле',
+                    $dataPosition = $this.data('tooltip-pos'),
+                    position = {};
+
+                // устанавливаем позицию тултипа в зависимости от содержимого data-tooltip-pos
+                position.my = $dataPosition === 'left' ? 'center right' : 'center left';
+                position.at = $dataPosition === 'left' ? 'center left' : 'center right';
+                // если текущее поле является скрытым input[type="file"], то позиционируем тултип
+                // относительно родительского блока
+                position.target = $this.hasClass('input-file__hidden') ? $this.parent() : $this;
+
+                $this.qtip({
+                    content: {
+                        text: $text
+                    },
+                    position: {
+                        my: position.my,
+                        at: position.at,
+                        target: position.target
+                    },
+                    show: {
+                        event: 'validationFail' // тултип будет показан на кастомное событие validationFail
+                    },
+                    hide: {
+                        event: 'validation' // тултип будет спрятан на кастомное событие validation
+                    },
+                    style: {
+                        classes: 'qtip-rounded tooltip'
+                    }
+                });
+
+            });
+
+        }
+
+    }
+
+    // Функция AJAX отправки формы
+    // В начале функция валидирует форму,
+    // затем отправляет данные из формы на указанный url
+    var ajaxForm = function($form, url) {
+
+        // получаем все поля формы, ищем среди них те,
+        // у которых есть атрибут required и валидируем их
+        var $formFields = $form.find(':input'),
+            $requiredFields = $formFields.filter('[required]'),
+            valid = validateFields($requiredFields);
+
+        if (!valid) {
+            return false;
+        }
+
+        var data = $form.serializeArray();
+
+        var result = $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            data: data
+        });
+
+        return result;
+
+    };
+
+    // Добавляет всем формам атрибут novalidate для отмены браузерной валидации
+    // и вешает обработчик на submit
+    var _setupForms = function() {
+
+        var $form = $('.form');
+
+        if ($form.length) {
+            $form.attr('novalidate', '');
+
+            $form.on('submit', _submitForm);
+        }
+
+    }
+
+    // функция-обработчик события сабмит формы
+    var _submitForm = function(e) {
+
+        var $this = $(this);
+
+        e.preventDefault();
+
+        ajaxForm($this);
+
+    }
+
+    // Добавляем название загруженного пользователем файла в кастомный input[type="file"]
+    var _styleInputFile = function() {
+
+        var inputFile = $('.input-file');
+
+        if (inputFile.length) {
+
+            inputFile.each( function() {
+
+                var $this = $(this),
+                    $defaultVal = $this.text(),
+                    $textContainer = $this.find('.input-file__text'),
+                    $hiddenInput = $this.find('.input-file__hidden');
+
+                $hiddenInput.on('change', function(e) {
+
+                    var fileName = e.target.value.split('\\').pop();
+
+                    if(fileName) {
+                        $textContainer.text(fileName);
+
+                        if (!$this.hasClass('input-file_non-empty')) {
+                            $this.addClass('input-file_non-empty');
+                        }
+                    }
+
+                });
+
+            });
+
+        }
+
+    }
+
+    // Добавляем placeholder в те браузеры, где он не поддерживается
+    var _addPlaceholdersToOldBrowsers = function() {
+
+        if ($('input, textarea').length) {
+            $('input, textarea').placeholder();
+        }
+
+    };
+
+    return {
+        init: init,
+        validateFields: validateFields,
+        ajaxForm: ajaxForm
+    }
+
+})();
+
 $(function() {
 
     app.init();
+    formsModule.init();
 
 });
